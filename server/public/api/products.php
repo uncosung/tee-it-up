b617ce90989bf6a0da79f9b3bacd9fbf2970eae9
uncosung <eric.k.sung@gmail.com>
2019-05-21 23:43:56 +0000

    Removed commented out code

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -1,14 +1,3 @@
-<!-- <?php
-
-// header('Content-Type: application/json');
-
-// if (empty($_GET['id'])) {
-//   readfile('dummy-products-list.json');
-// } else {
-//   readfile('dummy-product-details.json');
-// }
-
-?> -->
 
 <?php 
   require_once 'functions.php';
