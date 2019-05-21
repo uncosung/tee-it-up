90cab54d46b71477c5d5a40dc931829fd0461407
uncosung <eric.k.sung@gmail.com>
2019-05-21 22:19:02 +0000

    upstream changes

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -1,11 +1,18 @@
-<?php
+<!-- <?php
 
-header('Content-Type: application/json');
+// header('Content-Type: application/json');
 
-if (empty($_GET['id'])) {
-  readfile('dummy-products-list.json');
-} else {
-  readfile('dummy-product-details.json');
-}
+// if (empty($_GET['id'])) {
+//   readfile('dummy-products-list.json');
+// } else {
+//   readfile('dummy-product-details.json');
+// }
 
+?> -->
+
+<?php 
+  require_once 'functions.php';
+  set_exception_handler('error_handler');
+  $output = file_get_contents('dummy-products-list.json');
+  print($output);
 ?>
