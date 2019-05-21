2297422febf7a5de36d61d9c45d65685d74815df
uncosung <eric.k.sung@gmail.com>
2019-05-21 23:54:44 +0000

    Completed be_connect_products_to_mysql feature

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -1,6 +1,7 @@
 
 <?php 
   require_once 'functions.php';
+  require_once 'db_connection.php';
   set_exception_handler('error_handler');
   $output = file_get_contents('dummy-products-list.json');
   print($output);
