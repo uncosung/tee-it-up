da0486114fc7eae6ab473dd160e2d9dfe7018b11
uncosung <eric.k.sung@gmail.com>
2019-05-22 17:33:51 +0000

    Worked on changes requested in pull request review, got product details feature working with backend data

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -3,11 +3,17 @@
   require_once 'functions.php';
   require_once 'db_connection.php';
   set_exception_handler('error_handler');
+  startup();
   if (!$conn) {
     throw new Exception(mysqli_connect_error($conn));
     exit();
   }
+  if (empty($_GET['id'])){
   $query = "SELECT * FROM `wicked-sales-products`";
+  }
+  else {
+  $query = "SELECT * FROM `wicked-sales-products` WHERE `id` = {$_GET['id']}";
+  }
   $result = mysqli_query($conn, $query);
   if(!$result) {
     throw new Exception (mysqli_error($conn));
