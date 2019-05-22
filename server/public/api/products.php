3871d7332b0f77109303773c5ca35e10379af84b
uncosung <eric.k.sung@gmail.com>
2019-05-22 01:32:04 +0000

    Completed be_connect_products_to_mysql feature

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -3,6 +3,23 @@
   require_once 'functions.php';
   require_once 'db_connection.php';
   set_exception_handler('error_handler');
-  $output = file_get_contents('dummy-products-list.json');
-  print($output);
+  if (!$conn) {
+    throw new Exception('Error: ' . mysqli_connect_error($conn));
+  }
+  $query = "SELECT `id`, `name`, `price`, `image`, `shortDescription` FROM `wicked-sales-products`";
+  $result = mysqli_query($conn, $query);
+  if(!$result) {
+    print('Error: ' . mysqli_error($conn));
+    exit();
+  }
+  $output = [];
+  if (mysqli_num_rows($result) === 0) {
+    print('No data available');
+    exit();
+  }
+  while ($row = mysqli_fetch_assoc($result)){
+    $output[] = $row;
+  }
+  $json_output = json_encode($output);
+  print($json_output);
 ?>
